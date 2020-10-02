8f136310b2e433c7c3d5206c7d216fa4f63a6a96
rajs222000 <71223291+rajs222000@users.noreply.github.com>
2020-10-02 16:08:52 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,43 @@
+import random
+
+while True:
+    comp_list = ["Rock","rock","Paper","paper","Scissor","scissor"]
+    user = input("Welcome, please choose an option :").capitalize()
+    if user in comp_list:
+        pass
+    else:
+        print("You have written a wrong input ")
+        continue
+    comp = random.choice(comp_list)
+    
+    #print
+    print(f"You choose {user}")
+    print(f"Computer choose {comp}")
+    
+    #main
+    if user == "Rock" and comp == "Scissor":
+        print("You Won!")
+    elif user == "Rock" and comp == "Paper":
+        print("Compurt Wins!")
+    elif user == "Rock" and comp == "Rock":
+        print("It's a TIE! please try again:")
+    
+    elif user == "Paper" and comp == "Scissor":
+        print("Computer Wins!")
+    elif user == "Paper" and comp == "Paper":
+        print("It's a TIE! please try again:")
+    elif user == "Paper" and comp == "Rock":
+        print("You Won!")
+        
+    elif user == "Scissor" and comp == "Scissor":
+        print("It's a TIE! please try again:")
+    elif user == "Scissor" and comp == "Paper":
+        print("You Won!")
+    elif user == "Scissor" and comp == "Rock":
+        print("Computer Wins")
+        
+    repeat = input("Do you want to continue ?(Y/N)\n").capitalize()
+    if "Y" in repeat:
+        continue
+    else:
+        exit()
